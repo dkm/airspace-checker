8e3fdb958ad51ce03e1979881b1afb511a52b78c
Marc Poulhiès <dkm@kataplop.net>
2011-02-03 18:06:02 +0100

    better frontend for OpenAir compiler

--- bin/oair2shp.py
+++ bin/oair2shp.py
@@ -22,5 +22,38 @@ import airspace.shp
 
 import sys
 
-res = airspace.parser.parse(sys.argv[1])
-shp.writeToShp(sys.argv[2], res)
+import argparse
+import os
+
+def main():
+    parser = argparse.ArgumentParser(description='Compile OpenAIR file into ESRI Shapefile.')
+    parser.add_argument('--openair', metavar='OpenAir', type=str, 
+                        help='The OpenAIR file to compile')
+    parser.add_argument('--shapefile', metavar='ShapefileName', type=str, 
+                        help='the output ESRI Shapefile base prefix')
+    parser.add_argument('--force', action="store_true", default=False, 
+                        help='overwrite output if it exists')
+
+    args = parser.parse_args()
+
+    already_warned = False
+    for ext in [".shp", ".shx", ".dbf"]:
+        if os.path.exists(args.shapefile + ext):
+            if args.force:
+                if not already_warned:
+                    print "Overwriting existing zones"
+                    already_warned = True
+                os.remove(args.shapefile + ext)
+            else:
+                print "exiting. Use --force to overwrite"
+                return
+
+    res = airspace.parser.parse(args.openair)
+    if not res:
+        print "Parser returned 0 zones, not writing anything."
+    else:
+        print "Found %d zones" %len(res)
+        airspace.shp.writeToShp(args.shapefile, res)
+
+if __name__ == "__main__":
+    main()
