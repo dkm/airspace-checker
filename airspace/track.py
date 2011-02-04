cc04db229e9d175ebcbd4f00942650af5fdafe14
Marc Poulhiès <dkm@kataplop.net>
2011-02-04 09:46:14 +0100

    move GPX loading to track module

--- airspace/track.py
+++ airspace/track.py
@@ -17,9 +17,18 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# For the loadFromGpx() method, mainly based on ogr2osm.py:
+#####################################################################################
+#   "THE BEER-WARE LICENSE":                                                        #
+#   <ivan@sanchezortega.es> wrote this file. As long as you retain this notice you  #
+#   can do whatever you want with this stuff. If we meet some day, and you think    #
+#   this stuff is worth it, you can buy me a beer in return.                        #
+#####################################################################################
+
 import re
 import util
 import osgeo.ogr
+import shapely.wkt
 
 def loadSimpleTxt(txtfile):
     fin = open(txtfile, "r")
@@ -33,29 +42,74 @@ def loadSimpleTxt(txtfile):
     fin.close()
     return ls
 
-def loadGpx(gpxname):
+def loadFromGpxToShapely(gpxfilename, detectProjection=True):
     """
-    Match lines like:
-    <trkpt lat="45.129961" lon="5.883522">
-    in the GPX file. Very weak.
+    From a GPX, loads all lines/multilines and returns
+    a list of shapely representation
     """
-    fin = open(gpxname, "r")
-    ls = osgeo.ogr.Geometry(osgeo.ogr.wkbLineString)
-    cur_point = None
+    return [shapely.wkt.loads(x) for x in loadFromGpx(gpxfilename, detectProjection)]
 
-    for line in fin.xreadlines():
-        m = re.search('<trkpt lat="(?P<lat>\d+\.\d+)" lon="(?P<lon>\d+\.\d+)">', line)
-        if m != None:
-            lat = m.group('lat')
-            lon = m.group('lon')
-            cur_coord = [lat, lon]
+def loadFromGpx(gpxfilename, detectProjection=True):
+    """
+    From a GPX, loads all lines/multilines and returns
+    WKT representation
+    """
+    driver = osgeo.ogr.GetDriverByName('GPX')
+    dataSource = driver.Open(gpxfilename, 0)
+    geoms_to_return = []
 
-        m = re.search('<ele>(?P<elevation>\d+\.\d+)</ele>', line)
-        if m != None:
-            ele = m.group('elevation')
-            cur_coord.append(ele)
+    for i in xrange(dataSource.GetLayerCount()):
+	layer = dataSource.GetLayer(i)
+	layer.ResetReading()
+	
+	spatialRef = None
+	if detectProjection:
+            spatialRef = layer.GetSpatialRef()
+	# elif useProj4:
+	# 	spatialRef = osr.SpatialReference()
+	# 	spatialRef.ImportFromProj4(sourceProj4)
 
-            ls.AddPoint(float(cur_coord[1]), 
-                        float(cur_coord[0]),
-                        float(ele))
-    return ls
+            
+	if spatialRef == None:	
+            # No source proj specified yet? Then default to do no reprojection.
+            # Some python magic: skip reprojection
+            # altogether by using a dummy lamdba
+            # funcion. Otherwise, the lambda will
+            # be a call to the OGR reprojection
+            # stuff.
+            reproject = lambda(geometry): None
+	else:
+            destSpatialRef = osgeo.osr.SpatialReference()
+            destSpatialRef.ImportFromEPSG(4326)	
+            # Destionation projection will *always* be EPSG:4326,
+            # WGS84 lat-lon
+            coordTrans = osgeo.osr.CoordinateTransformation(spatialRef,destSpatialRef)
+            reproject = lambda(geometry): geometry.Transform(coordTrans)
+
+        featureDefinition = layer.GetLayerDefn()
+
+	for j in range(layer.GetFeatureCount()):
+            feature = layer.GetNextFeature()
+            geometry = feature.GetGeometryRef()
+            geometryType = geometry.GetGeometryType()
+            
+            ls = []
+
+            if  geometryType == osgeo.ogr.wkbLineString or geometryType == osgeo.ogr.wkbLineString25D:
+                if geometry.GetPointCount() <= 2:
+                    continue
+                ls.append(geometry)
+            elif geometryType == osgeo.ogr.wkbMultiLineString or geometryType == osgeo.ogr.wkbMultiLineString25D:
+                for i in xrange(geometry.GetGeometryCount()):
+                    subgeom = geometry.GetGeometryRef(i)
+                    if subgeom.GetPointCount() <= 2:
+                        continue
+                    else:
+                        ls.append(subgeom)
+                for line in ls:
+                    reproject(line)
+                    geoms_to_return.append(line.ExportToWkt())
+
+    return geoms_to_return
+
+
